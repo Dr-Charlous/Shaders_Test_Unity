7324dcfe0bfb31cf5256ae90b44890ce1e77456e
Piercourt Charles <113095075+Dr-Charlous@users.noreply.github.com>
2025-03-07 12:07:57 +0100

    Shader change colors and others exercices

---
+++ Assets/Script.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    //public Material Material;
+    //public Vector3 Move;
+
+    //private void Update()
+    //{
+    //    Move = Material.GetVector("_Offset");
+    //    Move += Vector3.up;
+    //    Material.SetVector("_Offset", Move);
+    //}
+}
